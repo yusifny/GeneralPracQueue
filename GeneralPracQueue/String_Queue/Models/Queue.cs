03a1595f193a831e71b0dc2499b07e8e216ecb03
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-05-28 17:58:21 +0400

    Add files via upload

---
+++ GeneralPracQueue/String_Queue/Models/Queue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GeneralPracQueue.Models
+{
+    public class Queues
+    {
+        public Queue<string> Reverse(Queue<string> words)
+        {
+            Queue<string> queue = new Queue<string>();
+            Stack stack = new Stack();
+            foreach (var item in words)
+            {
+                stack.Push(item);
+            }
+            foreach (var item in queue)
+            {
+                words.Enqueue((string)item);
+            }
+            return queue;
+        }
+
+    }
+}
---
+++ GeneralPracQueue/String_Queue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GeneralPracQueue.Models;
+
+namespace GeneralPracQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Queues queues = new Queues();
+
+            stack.Reverse();
+
+        }
+    }
+}
